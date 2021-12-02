e35a05a69a6429690299e948f4c171366237f595
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-12-02 07:48:45 +0100

    Simplify solution

--- lib/aoc2021/day2.ex
+++ lib/aoc2021/day2.ex
@@ -1,62 +1,39 @@
 defmodule Aoc2021.Day2 do
-  defmodule Part1Position do
-    defstruct [:horizontal, :depth]
-
-    def initial, do: %__MODULE__{horizontal: 0, depth: 0}
-
-    def apply(position, "up", amount), do: Map.update!(position, :depth, &(&1 - amount))
-    def apply(position, "down", amount), do: Map.update!(position, :depth, &(&1 + amount))
-
-    def apply(position, "forward", amount),
-      do: Map.update!(position, :horizontal, &(&1 + amount))
-
-    def multiply(position), do: position.horizontal * position.depth
-  end
-
-  defmodule Part2Position do
-    defstruct [:horizontal, :depth, :aim]
-
-    def initial, do: %__MODULE__{horizontal: 0, depth: 0, aim: 0}
-
-    def apply(position, "up", amount), do: Map.update!(position, :aim, &(&1 - amount))
-    def apply(position, "down", amount), do: Map.update!(position, :aim, &(&1 + amount))
-
-    def apply(position, "forward", amount) do
-      %{
-        position
-        | horizontal: position.horizontal + amount,
-          depth: position.depth + position.aim * amount
-      }
-    end
-
-    def multiply(position), do: position.horizontal * position.depth
-  end
-
-  alias __MODULE__.Part1Position
-  alias __MODULE__.Part2Position
-
   def part1(input) do
     input
     |> parse
     |> Enum.reduce(
-      Part1Position.initial(),
-      fn {command, amount}, position ->
-        Part1Position.apply(position, command, amount)
+      %{horizontal: 0, depth: 0},
+      fn
+        {"up", amount}, position -> %{position | depth: position.depth - amount}
+        {"down", amount}, position -> %{position | depth: position.depth + amount}
+        {"forward", amount}, position -> %{position | horizontal: position.horizontal + amount}
       end
     )
-    |> Part1Position.multiply()
+    |> then(fn position -> position.horizontal * position.depth end)
   end
 
   def part2(input) do
     input
     |> parse
     |> Enum.reduce(
-      Part2Position.initial(),
-      fn {command, amount}, position ->
-        Part2Position.apply(position, command, amount)
+      %{horizontal: 0, depth: 0, aim: 0},
+      fn
+        {"up", amount}, position ->
+          %{position | aim: position.aim - amount}
+
+        {"down", amount}, position ->
+          %{position | aim: position.aim + amount}
+
+        {"forward", amount}, position ->
+          %{
+            position
+            | horizontal: position.horizontal + amount,
+              depth: position.depth + position.aim * amount
+          }
       end
     )
-    |> Part2Position.multiply()
+    |> then(fn position -> position.horizontal * position.depth end)
   end
 
   defp parse(input) do
