e509c5b383edee8ce9a41216c3e173add7c47379
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-12-03 13:42:22 +0100

    Refactor day3 solution

--- lib/aoc2021/day3.ex
+++ lib/aoc2021/day3.ex
@@ -1,17 +1,16 @@
 defmodule Aoc2021.Day3 do
   def part1(input) do
+    numbers = parse(input)
+
     digits_count =
-      input
-      |> parse()
+      numbers
       |> List.first()
-      |> String.length()
+      |> Enum.count()
 
     gamma_rate =
-      input
-      |> parse()
+      numbers
       |> most_frequent_digits()
-      |> Enum.join("")
-      |> String.to_integer(2)
+      |> to_int()
 
     epsilon_rate = :math.pow(2, digits_count) - 1 - gamma_rate
 
@@ -19,69 +18,53 @@ defmodule Aoc2021.Day3 do
   end
 
   def part2(input) do
-    input = parse(input)
-
-    digits_count =
-      input
-      |> List.first()
-      |> String.length()
-
-    numbers =
-      input
-      |> Enum.map(fn number ->
-        number
-        |> String.graphemes()
-        |> Enum.map(&String.to_integer/1)
-      end)
+    numbers = parse(input)
 
     oxygen_generator_rating =
-      0..digits_count
-      |> Enum.reduce_while(
-        numbers,
-        fn
-          _idx, [number] ->
-            {:halt, number}
-
-          idx, numbers ->
-            mfd = most_frequent_digits(Enum.map(numbers, &Enum.join(&1, "")))
-            {:cont, Enum.filter(numbers, fn s -> Enum.at(s, idx) == Enum.at(mfd, idx) end)}
-        end
-      )
-      |> Enum.join("")
-      |> String.to_integer(2)
+      numbers
+      |> filter_values(fn numbers, idx -> most_frequent_digits(numbers) |> Enum.at(idx) end)
+      |> to_int()
 
     co2_scrubber_rating =
-      0..digits_count
-      |> Enum.reduce_while(
-        numbers,
-        fn
-          _idx, [number] ->
-            {:halt, number}
+      numbers
+      |> filter_values(fn numbers, idx ->
+        most_frequent_digits(numbers) |> Enum.map(&(1 - &1)) |> Enum.at(idx)
+      end)
+      |> to_int()
 
-          idx, numbers ->
-            mfd = most_frequent_digits(Enum.map(numbers, &Enum.join(&1, ""))) |> Enum.map(&(1 - &1))
-            {:cont, Enum.filter(numbers, fn s -> Enum.at(s, idx) == Enum.at(mfd, idx) end)}
-        end
-      )
-      |> Enum.join("")
-      |> String.to_integer(2)
-
-      oxygen_generator_rating * co2_scrubber_rating
+    oxygen_generator_rating * co2_scrubber_rating
   end
 
   defp parse(input) do
     input
     |> String.split()
-  end
-
-  defp group_digits(binary_nums) do
-    binary_nums
     |> Enum.map(fn number ->
       number
       |> String.graphemes()
       |> Enum.map(&String.to_integer/1)
     end)
-    |> Enum.zip()
+  end
+
+  defp group_digits(binary_nums), do: Enum.zip(binary_nums)
+
+  defp filter_values(numbers, target_digit_fun) do
+    digits_count =
+      numbers
+      |> List.first()
+      |> Enum.count()
+
+    0..digits_count
+    |> Enum.reduce_while(
+      numbers,
+      fn
+        _idx, [number] ->
+          {:halt, number}
+
+        idx, numbers ->
+          {:cont,
+           Enum.filter(numbers, fn num -> Enum.at(num, idx) == target_digit_fun.(numbers, idx) end)}
+      end
+    )
   end
 
   defp most_frequent_digits(binaries) do
@@ -91,5 +74,11 @@ defmodule Aoc2021.Day3 do
     |> group_digits()
     |> Enum.map(&Tuple.sum/1)
     |> Enum.map(fn ones_count -> if ones_count >= input_len / 2, do: 1, else: 0 end)
+  end
+
+  defp to_int(binary_digits) do
+    binary_digits
+    |> Enum.join("")
+    |> String.to_integer(2)
   end
 end
