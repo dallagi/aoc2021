293eabe1285bf73a580afc452d87605f16fc6c88
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-12-08 10:37:06 +0100

    Refactor day8

--- lib/aoc2021/day8.ex
+++ lib/aoc2021/day8.ex
@@ -27,62 +27,56 @@ defmodule Aoc2021.Day8 do
 
   def decode_patterns(patterns) do
     patterns_sets = Enum.map(patterns, &(&1 |> String.graphemes() |> MapSet.new()))
-    by_length = Enum.group_by(patterns_sets, &MapSet.size/1)
+    by_cardinality = Enum.group_by(patterns_sets, &MapSet.size/1)
+    first_by_cardinality = fn card -> get_in(by_cardinality, [card, Access.at(0)]) end
 
-    first_by_cardinality = fn card -> get_in(by_length, [card, Access.at(0)]) end
+    one = first_by_cardinality.(2)
+    seven = first_by_cardinality.(3)
+    four = first_by_cardinality.(4)
+    eight = first_by_cardinality.(7)
 
     g =
-      Enum.find_value(by_length[5], fn pattern ->
-        # The #5 pattern that has only 1 letter not included in #2, #3 and #4 patterns
-        letters_to_exclude =
-          first_by_cardinality.(2)
-          |> MapSet.union(first_by_cardinality.(3))
-          |> MapSet.union(first_by_cardinality.(4))
+      Enum.find_value(by_cardinality[5], fn pattern ->
+        # The #5 pattern that has only 1 letter not included in one, four and seven
+        to_exclude = one |> MapSet.union(four) |> MapSet.union(seven)
+        difference = MapSet.difference(pattern, to_exclude)
 
-        difference = MapSet.difference(pattern, letters_to_exclude)
-
-        if MapSet.size(difference) == 1,
-          do: difference |> MapSet.to_list() |> List.first(),
-          else: nil
+        if MapSet.size(difference) == 1, do: difference |> MapSet.to_list() |> List.first()
       end)
 
-    # letter not included in (#2 + #3 + #4 + {g})
-    letters_to_exclude =
-      first_by_cardinality.(2)
-      |> MapSet.union(first_by_cardinality.(3))
-      |> MapSet.union(first_by_cardinality.(4))
-      |> MapSet.put(g)
+    # letter not included in (one + seven + four + {g})
+    not_e = one |> MapSet.union(seven) |> MapSet.union(four) |> MapSet.put(g)
 
     e =
       ~w(a b c d e f g)
       |> MapSet.new()
-      |> MapSet.difference(letters_to_exclude)
+      |> MapSet.difference(not_e)
       |> MapSet.to_list()
       |> List.first()
 
     # #5 pattern that includes 'e'
-    two = Enum.find(by_length[5], &MapSet.member?(&1, e))
+    two = Enum.find(by_cardinality[5], &MapSet.member?(&1, e))
 
-    # #5 pattern that is superset of #2
-    three = Enum.find(by_length[5], &MapSet.subset?(first_by_cardinality.(2), &1))
+    # #5 pattern that is superset of one
+    three = Enum.find(by_cardinality[5], &MapSet.subset?(one, &1))
 
     # #5 pattern that is not two or three
-    five = Enum.find(by_length[5], &(&1 != two && &1 != three))
+    five = Enum.find(by_cardinality[5], &(&1 != two && &1 != three))
 
     # #6 pattern that does not include 'e'
-    nine = Enum.find(by_length[6], &(!MapSet.member?(&1, e)))
+    nine = Enum.find(by_cardinality[6], &(!MapSet.member?(&1, e)))
 
-    # #6 pattern that is not nine and includes #2
-    zero = Enum.find(by_length[6], &(&1 != nine and MapSet.subset?(first_by_cardinality.(2), &1)))
+    # #6 pattern that is not nine and includes one
+    zero = Enum.find(by_cardinality[6], &(&1 != nine and MapSet.subset?(one, &1)))
 
     # remaining pattern
-    six = Enum.find(by_length[6], &(&1 != nine and &1 != zero))
+    six = Enum.find(by_cardinality[6], &(&1 != nine and &1 != zero))
 
     %{
-      first_by_cardinality.(2) => 1,
-      first_by_cardinality.(3) => 7,
-      first_by_cardinality.(4) => 4,
-      first_by_cardinality.(7) => 8,
+      one => 1,
+      seven => 7,
+      four => 4,
+      eight => 8,
       two => 2,
       three => 3,
       five => 5,
