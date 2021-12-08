ba07485be5607f1ef49f2c16f062d7525607a82b
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-12-08 10:14:07 +0100

    Solve day8 part 2 - the convoluted way

--- lib/aoc2021/day8.ex
+++ lib/aoc2021/day8.ex
@@ -11,7 +11,85 @@ defmodule Aoc2021.Day8 do
   end
 
   def part2(input) do
-    nil
+    input
+    |> parse
+    |> Enum.map(fn {patterns, output} ->
+      decoder = decode_patterns(patterns)
+      decode = fn pattern -> decoder[MapSet.new(String.graphemes(pattern))] end
+
+      output
+      |> Enum.map(decode)
+      |> Enum.join()
+      |> String.to_integer()
+    end)
+    |> Enum.sum()
+  end
+
+  def decode_patterns(patterns) do
+    patterns_sets = Enum.map(patterns, &(&1 |> String.graphemes() |> MapSet.new()))
+    by_length = Enum.group_by(patterns_sets, &MapSet.size/1)
+
+    first_by_cardinality = fn card -> get_in(by_length, [card, Access.at(0)]) end
+
+    g =
+      Enum.find_value(by_length[5], fn pattern ->
+        # The #5 pattern that has only 1 letter not included in #2, #3 and #4 patterns
+        letters_to_exclude =
+          first_by_cardinality.(2)
+          |> MapSet.union(first_by_cardinality.(3))
+          |> MapSet.union(first_by_cardinality.(4))
+
+        difference = MapSet.difference(pattern, letters_to_exclude)
+
+        if MapSet.size(difference) == 1,
+          do: difference |> MapSet.to_list() |> List.first(),
+          else: nil
+      end)
+
+    # letter not included in (#2 + #3 + #4 + {g})
+    letters_to_exclude =
+      first_by_cardinality.(2)
+      |> MapSet.union(first_by_cardinality.(3))
+      |> MapSet.union(first_by_cardinality.(4))
+      |> MapSet.put(g)
+
+    e =
+      ~w(a b c d e f g)
+      |> MapSet.new()
+      |> MapSet.difference(letters_to_exclude)
+      |> MapSet.to_list()
+      |> List.first()
+
+    # #5 pattern that includes 'e'
+    two = Enum.find(by_length[5], &MapSet.member?(&1, e))
+
+    # #5 pattern that is superset of #2
+    three = Enum.find(by_length[5], &MapSet.subset?(first_by_cardinality.(2), &1))
+
+    # #5 pattern that is not two or three
+    five = Enum.find(by_length[5], &(&1 != two && &1 != three))
+
+    # #6 pattern that does not include 'e'
+    nine = Enum.find(by_length[6], &(!MapSet.member?(&1, e)))
+
+    # #6 pattern that is not nine and includes #2
+    zero = Enum.find(by_length[6], &(&1 != nine and MapSet.subset?(first_by_cardinality.(2), &1)))
+
+    # remaining pattern
+    six = Enum.find(by_length[6], &(&1 != nine and &1 != zero))
+
+    %{
+      first_by_cardinality.(2) => 1,
+      first_by_cardinality.(3) => 7,
+      first_by_cardinality.(4) => 4,
+      first_by_cardinality.(7) => 8,
+      two => 2,
+      three => 3,
+      five => 5,
+      nine => 9,
+      zero => 0,
+      six => 6
+    }
   end
 
   defp parse(input) do
--- test/aoc2021/day8_test.exs
+++ test/aoc2021/day8_test.exs
@@ -2,23 +2,40 @@ defmodule Aoc2021.Day8Test do
   use ExUnit.Case, async: true
 
   @example_input """
-    be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
-    edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
-    fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
-    fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
-    aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
-    fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
-    dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
-    bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
-    egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
-    gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
-    """
+  be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+  edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+  fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+  fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+  aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+  fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+  dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+  bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+  egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+  gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+  """
 
   test "solves part1 for provided example" do
     assert 26 == Aoc2021.Day8.part1(@example_input)
   end
 
+  test "decodes digits from patterns" do
+    example_patterns = ~w(acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab)
+
+    assert %{
+             MapSet.new(~w(a c e d g f b)) => 8,
+             MapSet.new(~w(c d f b e)) => 5,
+             MapSet.new(~w(g c d f a)) => 2,
+             MapSet.new(~w(f b c a d)) => 3,
+             MapSet.new(~w(d a b)) => 7,
+             MapSet.new(~w(c e f a b d)) => 9,
+             MapSet.new(~w(c d f g e b)) => 6,
+             MapSet.new(~w(e a f b)) => 4,
+             MapSet.new(~w(c a g e d b)) => 0,
+             MapSet.new(~w(a b)) => 1
+           } == Aoc2021.Day8.decode_patterns(example_patterns)
+  end
+
   test "solves part2 for provided example" do
-    # assert nil == Aoc2021.Day8.part2(@example_input)
+    assert 61229 == Aoc2021.Day8.part2(@example_input)
   end
 end
