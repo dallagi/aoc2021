e49bb0420a021073751f9256cd4c3c657160be57
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-12-10 08:30:46 +0100

    Solve day10 part2

--- lib/aoc2021/day10.ex
+++ lib/aoc2021/day10.ex
@@ -12,31 +12,51 @@ defmodule Aoc2021.Day10 do
   end
 
   def part2(input) do
-    nil
+    input
+    |> parse
+    |> Enum.map(&check_line/1)
+    |> Enum.filter(&(elem(&1, 0) == :incomplete))
+    |> Enum.map(&autocomplete_score/1)
+    |> median()
   end
 
   defp check_line(line, open_delimiters \\ []) do
     case line do
       [] ->
-        :ok
+        {:incomplete, open_delimiters}
 
       [delimiter | rest] when delimiter in @opening_delimiters ->
         check_line(rest, [delimiter | open_delimiters])
 
       [delimiter | rest] when delimiter in @closing_delimiters ->
         if Enum.empty?(open_delimiters) or delimiter != @delimiters[hd(open_delimiters)] do
-          {:syntax_error, delimiter, open_delimiters}
+          {:syntax_error, delimiter}
         else
           check_line(rest, tl(open_delimiters))
         end
     end
   end
 
-  defp syntax_score(:ok), do: 0
-  defp syntax_score({:syntax_error, ")", _}), do: 3
-  defp syntax_score({:syntax_error, "]", _}), do: 57
-  defp syntax_score({:syntax_error, "}", _}), do: 1197
-  defp syntax_score({:syntax_error, ">", _}), do: 25137
+  defp syntax_score({:syntax_error, ")"}), do: 3
+  defp syntax_score({:syntax_error, "]"}), do: 57
+  defp syntax_score({:syntax_error, "}"}), do: 1197
+  defp syntax_score({:syntax_error, ">"}), do: 25137
+  defp syntax_score(_), do: 0
+
+  def autocomplete_score({:incomplete, open_delimiters}) do
+    value = %{"(" => 1, "[" => 2, "{" => 3, "<" => 4}
+
+    Enum.reduce(open_delimiters, 0, fn delimiter, score ->
+      score * 5 + value[delimiter]
+    end)
+  end
+
+  def autocomplete_score(_), do: 0
+
+  defp median(enumerable) do
+    idx = div(length(enumerable), 2)
+    Enum.at(Enum.sort(enumerable), idx)
+  end
 
   defp parse(input) do
     input |> String.split("\n", trim: true) |> Enum.map(&String.split(&1, "", trim: true))
