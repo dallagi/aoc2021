a356c798f5f89681bc57a9a5d62dc76ce6a17439
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-12-10 08:12:43 +0100

    Refactor day10 to prepare for part2

--- lib/aoc2021/day10.ex
+++ lib/aoc2021/day10.ex
@@ -6,7 +6,8 @@ defmodule Aoc2021.Day10 do
   def part1(input) do
     input
     |> parse
-    |> Enum.map(&line_syntax_score/1)
+    |> Enum.map(&check_line/1)
+    |> Enum.map(&syntax_score/1)
     |> Enum.sum()
   end
 
@@ -14,27 +15,28 @@ defmodule Aoc2021.Day10 do
     nil
   end
 
-  def line_syntax_score(line, open_delimiters \\ []) do
+  defp check_line(line, open_delimiters \\ []) do
     case line do
       [] ->
-        0
+        :ok
 
       [delimiter | rest] when delimiter in @opening_delimiters ->
-        line_syntax_score(rest, [delimiter | open_delimiters])
+        check_line(rest, [delimiter | open_delimiters])
 
       [delimiter | rest] when delimiter in @closing_delimiters ->
         if Enum.empty?(open_delimiters) or delimiter != @delimiters[hd(open_delimiters)] do
-          syntax_score_for(delimiter)
+          {:syntax_error, delimiter, open_delimiters}
         else
-          line_syntax_score(rest, tl(open_delimiters))
+          check_line(rest, tl(open_delimiters))
         end
     end
   end
 
-  defp syntax_score_for(")"), do: 3
-  defp syntax_score_for("]"), do: 57
-  defp syntax_score_for("}"), do: 1197
-  defp syntax_score_for(">"), do: 25137
+  defp syntax_score(:ok), do: 0
+  defp syntax_score({:syntax_error, ")", _}), do: 3
+  defp syntax_score({:syntax_error, "]", _}), do: 57
+  defp syntax_score({:syntax_error, "}", _}), do: 1197
+  defp syntax_score({:syntax_error, ">", _}), do: 25137
 
   defp parse(input) do
     input |> String.split("\n", trim: true) |> Enum.map(&String.split(&1, "", trim: true))
--- test/aoc2021/day10_test.exs
+++ test/aoc2021/day10_test.exs
@@ -1,6 +1,8 @@
 defmodule Aoc2021.Day10Test do
   use ExUnit.Case, async: true
 
+  alias Aoc2021.Day10
+
   @example_input """
   [({(<(())[]>[[{[]{<()<>>
   [(()[<>])]({[<{<<[]>>(
@@ -14,19 +16,11 @@ defmodule Aoc2021.Day10Test do
   <{([{{}}[<[[[<>{}]]]>[]]
   """
 
-  test "calculates syntax score for line" do
-    example1 = String.split("{([(<{}[<>[]}>{[]{[(<()>", "", trim: true)
-    assert 1197 == Aoc2021.Day10.line_syntax_score(example1)
-
-    example2 = String.split("[[<[([]))<([[{}[[()]]]", "", trim: true)
-    assert 3 == Aoc2021.Day10.line_syntax_score(example2)
-  end
-
   test "solves part1 for provided example" do
-    assert 26397 == Aoc2021.Day10.part1(@example_input)
+    assert 26397 == Day10.part1(@example_input)
   end
 
   test "solves part2 for provided example" do
-    # assert nil == Aoc2021.Day10.part2(@example_input)
+    # assert nil == Day10.part2(@example_input)
   end
 end
