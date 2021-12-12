ad9982ffa39bf21c3c816643fa5e934fd830e750
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-12-12 10:59:06 +0100

    Solve day12 part2, the ugly way

--- lib/aoc2021/day12.ex
+++ lib/aoc2021/day12.ex
@@ -1,14 +1,14 @@
 defmodule Aoc2021.Day12 do
   def part1(input) do
-    parse(input)
-
     input
     |> parse()
     |> paths_count()
   end
 
   def part2(input) do
-    nil
+    input
+    |> parse()
+    |> paths_count2()
   end
 
   defp paths_count(connections, position \\ "start", visited \\ MapSet.new()) do
@@ -18,8 +18,43 @@ defmodule Aoc2021.Day12 do
       1
     else
       neighbors
+      |> Enum.reject(&(&1 == "start"))
       |> Enum.reject(&(small_cave?(&1) and &1 in visited))
       |> Enum.map(&paths_count(connections, &1, MapSet.put(visited, position)))
+      |> Enum.sum()
+    end
+  end
+
+  defp paths_count2(
+         connections,
+         position \\ "start",
+         visited \\ MapSet.new(),
+         small_cave_visited_twice? \\ false
+       ) do
+    neighbors = Map.get(connections, position, [])
+
+    if position == "end" do
+      1
+    else
+      neighbors
+      |> Enum.reject(&(&1 == "start"))
+      |> Enum.map(fn neighbor ->
+        cond do
+          small_cave?(neighbor) and neighbor in visited and small_cave_visited_twice? ->
+            0
+
+          small_cave?(neighbor) and neighbor in visited ->
+            paths_count2(connections, neighbor, MapSet.put(visited, position), true)
+
+          true ->
+            paths_count2(
+              connections,
+              neighbor,
+              MapSet.put(visited, position),
+              small_cave_visited_twice?
+            )
+        end
+      end)
       |> Enum.sum()
     end
   end
@@ -30,9 +65,10 @@ defmodule Aoc2021.Day12 do
     for row <- String.split(input, "\n", trim: true), reduce: %{} do
       connections ->
         [from, to] = String.split(row, "-", parts: 2)
+
         connections
-          |> Map.update(from, [to], fn dests -> [to | dests] end)
-          |> Map.update(to, [from], fn dests -> [from | dests] end)
+        |> Map.update(from, [to], fn dests -> [to | dests] end)
+        |> Map.update(to, [from], fn dests -> [from | dests] end)
     end
   end
 end
--- test/aoc2021/day12_test.exs
+++ test/aoc2021/day12_test.exs
@@ -11,28 +11,32 @@ defmodule Aoc2021.Day12Test do
   b-end
   """
 
+  @larger_example_input """
+  dc-end
+  HN-start
+  start-kj
+  dc-start
+  dc-HN
+  LN-dc
+  HN-end
+  kj-sa
+  kj-HN
+  kj-dc
+  """
+
   test "solves part1 for provided example" do
     assert 10 == Aoc2021.Day12.part1(@example_input)
   end
 
   test "solves part1 for larger provided example" do
-    input = """
-    dc-end
-    HN-start
-    start-kj
-    dc-start
-    dc-HN
-    LN-dc
-    HN-end
-    kj-sa
-    kj-HN
-    kj-dc
-    """
-
-    assert 19 == Aoc2021.Day12.part1(input)
+    assert 19 == Aoc2021.Day12.part1(@larger_example_input)
   end
 
   test "solves part2 for provided example" do
-    # assert nil == Aoc2021.Day12.part2(@example_input)
+    assert 36 == Aoc2021.Day12.part2(@example_input)
+  end
+
+  test "solves part2 for larger provided example" do
+    assert 103 == Aoc2021.Day12.part2(@larger_example_input)
   end
 end
