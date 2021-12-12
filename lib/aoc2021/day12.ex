ed35507cef47553e626f62885f96a5967ff92435
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-12-12 11:03:54 +0100

    Refactor day12

--- lib/aoc2021/day12.ex
+++ lib/aoc2021/day12.ex
@@ -2,61 +2,47 @@ defmodule Aoc2021.Day12 do
   def part1(input) do
     input
     |> parse()
-    |> paths_count()
+    |> paths_count(false)
   end
 
   def part2(input) do
     input
     |> parse()
-    |> paths_count2()
+    |> paths_count(true)
   end
 
-  defp paths_count(connections, position \\ "start", visited \\ MapSet.new()) do
-    neighbors = Map.get(connections, position, [])
-
-    if position == "end" do
-      1
-    else
-      neighbors
-      |> Enum.reject(&(&1 == "start"))
-      |> Enum.reject(&(small_cave?(&1) and &1 in visited))
-      |> Enum.map(&paths_count(connections, &1, MapSet.put(visited, position)))
-      |> Enum.sum()
-    end
-  end
-
-  defp paths_count2(
+  defp paths_count(
          connections,
+         can_visit_small_cave_twice?,
          position \\ "start",
-         visited \\ MapSet.new(),
-         small_cave_visited_twice? \\ false
-       ) do
+         visited \\ MapSet.new()
+       )
+
+  defp paths_count(_, _, "end", _), do: 1
+
+  defp paths_count(connections, can_visit_small_cave_twice?, position, visited) do
     neighbors = Map.get(connections, position, [])
 
-    if position == "end" do
-      1
-    else
-      neighbors
-      |> Enum.reject(&(&1 == "start"))
-      |> Enum.map(fn neighbor ->
-        cond do
-          small_cave?(neighbor) and neighbor in visited and small_cave_visited_twice? ->
-            0
+    neighbors
+    |> Enum.reject(&(&1 == "start"))
+    |> Enum.map(fn neighbor ->
+      cond do
+        small_cave?(neighbor) and neighbor in visited and can_visit_small_cave_twice? ->
+          paths_count(connections, false, neighbor, MapSet.put(visited, position))
 
-          small_cave?(neighbor) and neighbor in visited ->
-            paths_count2(connections, neighbor, MapSet.put(visited, position), true)
+        small_cave?(neighbor) and neighbor in visited ->
+          0
 
-          true ->
-            paths_count2(
-              connections,
-              neighbor,
-              MapSet.put(visited, position),
-              small_cave_visited_twice?
-            )
-        end
-      end)
-      |> Enum.sum()
-    end
+        true ->
+          paths_count(
+            connections,
+            can_visit_small_cave_twice?,
+            neighbor,
+            MapSet.put(visited, position)
+          )
+      end
+    end)
+    |> Enum.sum()
   end
 
   defp small_cave?(cave), do: cave =~ ~r/[a-z]+/
