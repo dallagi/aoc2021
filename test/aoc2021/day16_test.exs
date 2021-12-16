191eab3cf92258872fb9b21e75f64422163fbf85
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-12-16 09:53:41 +0100

    Clean up tests

--- test/aoc2021/day16_test.exs
+++ test/aoc2021/day16_test.exs
@@ -3,16 +3,6 @@ defmodule Aoc2021.Day16Test do
 
   alias Aoc2021.Day16
 
-  @example_input ""
-
-  test "solves part1 for provided example" do
-    # assert nil == Day16.part1(@example_input)
-  end
-
-  test "solves part2 for provided example" do
-    # assert nil == Day16.part2(@example_input)
-  end
-
   test "decodes literal value" do
     assert {{:literal, 6, 2021}, _} = "D2FE28" |> Base.decode16!() |> Day16.decode()
   end
